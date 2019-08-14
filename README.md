6f6d7a3329e593d5fe95d69be3d3b4a743ca91f2
haialaluf <hai.alaluf@gmail.com>
2019-08-14 18:34:40 +0300

    app build

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Hebrew calender לוח שנה עברי
 
-#### This is an hebrew calender mini web application that works fast, thanks to React, and works offline as well.
+#### This is an hebrew calender mini web application that works fast, and works offline as well.
---
+++ precache-manifest.51cad5cdf7e1a978b353cca82d9eb112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2b53ded576bfa0325bb5f2b293242c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0d8c1e3552f77c9d528",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "b5490d85d57595a4b369",
+    "url": "/static/css/main.7f70de8f.chunk.css"
+  },
+  {
+    "revision": "c0d8c1e3552f77c9d528",
+    "url": "/static/js/2.61017446.chunk.js"
+  },
+  {
+    "revision": "b5490d85d57595a4b369",
+    "url": "/static/js/main.ce2914bd.chunk.js"
+  },
+  {
+    "revision": "728046e91382a0b14846",
+    "url": "/static/js/runtime~main.51791c3b.js"
+  }
+]);
